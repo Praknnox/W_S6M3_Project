0429ea2799bdbd6f14ae52194cf36a54920a68c2
Pankorn Sirimanivong <sirimanivongp@gmail.com>
2024-11-01 15:18:20 -0700

    M4version

--- frontend/components/Card.js
+++ frontend/components/Card.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import Figure from './Figure'
+import styled from 'styled-components'
+
+const CardStyle=styled.div`
+p{margin:0 0 2rem 0}
+h2{font-size:1.7em;}
+padding:2rem;
+`
+
 export default function Card({title, text,image,date}){
     return(
-      <div>
+      <CardStyle>
           <h2>{title}</h2>
           <p>{text}</p>
           <Figure
           image={image}
           date={date}
           />
-        </div>
+        </CardStyle>
     )
   }
--- frontend/components/Figure.js
+++ frontend/components/Figure.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const FigureStyle=styled.figure`
+max-width:890px;
+padding-left:10rem;
+`
+
 export default function Figure({image, date}){
   return(
-        <figure>
+        <FigureStyle>
           <img src={image}/>
           <figcaption>{date}</figcaption>
-          </figure>
+          </FigureStyle>
 
   )
 }
